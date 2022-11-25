df66fe3510a7619c3ac5bf20c0ad15981e1bee56
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-11-26 01:18:31 +0530

    syncfusion graphs

--- components/Dashboard/salesInsights/SalesInsights.jsx
+++ components/Dashboard/salesInsights/SalesInsights.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 
-// import BarChart from './BarChart'
-// import Linechart from './Linechart'
+import BarChart from './BarChart'
+import Linechart from './Linechart'
 
 const SalesInsights = () => {
   return (
     <div className='flex flex-col items-center justify-center p-4 mt-4'>
       <p className='text-3xl font-bold text-gray-700'>Sales insights</p>
       <div className='grid w-full grid-cols-1 mt-12 md:grid-cols-2 gap-12'>
-          {/* <Linechart /> */}
-          {/* <BarChart /> */}
+          <Linechart />
+          <BarChart />
       </div>
     </div>
   )
